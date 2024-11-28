20c800d512a06e2e58e6982c4f534abb3a4c8606
Monalisha Roy <roymonalisha550@gmail.com>
2024-11-28 11:01:06 +0530

    uploadfile button

--- src/components/notes/uploadNotes.tsx
+++ src/components/notes/uploadNotes.tsx
@@ -6,14 +6,14 @@ export default function UploadNotes() {
   const [showPopup, setShowPopup] = useState(false);
 
     return (
-      <div className="w-full md:w-64 lg:w-72 h-80 rounded-lg bg-primary bg-opacity-50 flex flex-col gap-2 justify-center items-center text-text text-xl hover:bg-opacity-40 transition duration-300">
+      <div className="w-full md:w-64 lg:w-72 h-80 rounded-lg bg-[#789DBC] bg-opacity-50 flex flex-col gap-2 justify-center items-center text-text text-xl hover:bg-opacity-40 transition duration-300">
       {!showPopup ? (
         <>
           <div
             onClick={() => setShowPopup(true)}
-            className="text-text bg-secondary bg-opacity-50 rounded-full hover:scale-125 cursor-pointer transition duration-300"
+            className="text-text bg-[#003161] bg-opacity-40 rounded-full hover:scale-125 cursor-pointer transition duration-300"
           >
-            <FiPlus size={60} />
+            <FiPlus size={55} className="p-1"/>
           </div>
           <span>Upload your notes</span>
         </>
